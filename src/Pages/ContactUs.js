8f973c6aee032a50a0ec827098eed72aea41a0cc
SagnikGhosh001 <sagnikghosh904@gmail.com>
2024-06-12 00:26:12 +0530

    detailing

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -3,19 +3,9 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import Base from '../components/Base';
-
+import backgroundImg from "../resource/features.jpg";
 function ContactUs() {
-    const pageStyle = {
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f0f0f0',
-        padding: '2rem',
-        textAlign: 'center',
-        color: 'black',
-    };
+
     const headingStyle = {
         fontSize: '2.5rem',
         marginBottom: '1.5rem',
@@ -32,31 +22,46 @@ function ContactUs() {
     };
     return (
         <Base>
-            <div style={pageStyle}>
-                <Container>
-                    <Row className="justify-content-center">
-                        <Col md="6" className="text-center">
-                        <h1 style={headingStyle}>Contact Us</h1>
-                        <p style={paragraphStyle}>Connect with us on social media:</p>
-                            <Button color="primary" className="rounded-pill mx-1">
-                                <FontAwesomeIcon icon={faFacebook} />
-                            </Button>
-                            <Button color="primary" className="rounded-pill mx-1">
-                                <FontAwesomeIcon icon={faInstagram} />
-                            </Button>
-                            <Button color="primary" className="rounded-pill mx-1">
-                                <FontAwesomeIcon icon={faLinkedin} />
-                            </Button>
-                            <Button color="primary" className="rounded-pill mx-1">Gmail</Button>
-                            <Button color="primary" className="rounded-pill mx-1">
-                                <FontAwesomeIcon icon={faGithub} />
-                            </Button>
-                            <p style={contactInfoStyle}>Phone: +1234567890</p>
-                            <p style={contactInfoStyle}>Email: example@gmail.com</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            
+                <div style={{
+                backgroundImage: `url(${backgroundImg})`,
+                backgroundSize: 'cover',
+                padding: '2rem',
+                backgroundPosition: 'center',
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#f0f0f0',
+                textAlign: 'center',
+                color: 'black',
+            }}>
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col md="6" className="text-center">
+                                <h1 style={headingStyle}>Contact Us</h1>
+                                <p style={paragraphStyle}>Connect with us on social media:</p>
+                                <Button color="primary" className="rounded-pill mx-1">
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                </Button>
+                                <Button color="primary" className="rounded-pill mx-1">
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                </Button>
+                                <Button color="primary" className="rounded-pill mx-1">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </Button>
+                                <Button color="primary" className="rounded-pill mx-1">Gmail</Button>
+                                <Button color="primary" className="rounded-pill mx-1">
+                                    <FontAwesomeIcon icon={faGithub} />
+                                </Button>
+                                <p style={contactInfoStyle}>Phone: +1234567890</p>
+                                <p style={contactInfoStyle}>Email: example@gmail.com</p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            
         </Base>
     );
 }
--- src/Pages/Features.js
+++ src/Pages/Features.js
@@ -1,14 +1,17 @@
 import React, { useContext, useState } from 'react';
-import { Container, Row, Col, Button, Card, CardBody, CardTitle, CardText,NavLink } from 'reactstrap';
+import { Container, Row, Col, Button, Card, CardBody, CardTitle, CardText, NavLink } from 'reactstrap';
 import Base from '../components/Base';
 import { Link, NavLink as ReactLink, useNavigate } from 'react-router-dom';
 import userContext from '../context/userContext';
+import backgroundImg from "../resource/features6.jpg";
+
 function Features() {
     const object = useContext(userContext);
+
     // Inline styles
     const containerStyle = {
         marginTop: '40px',
-        backgroundColor: '#f8f9fa',
+        
         padding: '20px',
         borderRadius: '8px'
     };
@@ -16,12 +19,13 @@ function Features() {
     const headingStyle = {
         fontFamily: "'Open Sans', sans-serif",
         fontWeight: 700,
-        color: '#343a40',
+        color: 'white',
     };
 
     const paragraphStyle = {
         fontFamily: "'Roboto', sans-serif",
-        color: '#6c757d',
+        color: 'lightgray',
+        fontWeight: 700,
     };
 
     const cardStyle = {
@@ -30,7 +34,7 @@ function Features() {
         borderRadius: '8px',
         boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
         transition: 'transform 0.3s, box-shadow 0.3s',
-        backgroundColor: '#ffffff', 
+        backgroundColor: 'rgba(255, 255, 255, 0)', 
     };
 
     const cardHoverStyle = {
@@ -42,12 +46,12 @@ function Features() {
     const cardTitleStyle = {
         fontFamily: "'Open Sans', sans-serif",
         fontWeight: 600,
-        color: '#495057',
+        color: 'white',
     };
 
     const cardTextStyle = {
         fontFamily: "'Roboto', sans-serif",
-        color: '#6c757d',
+        color: 'lightgray',
     };
 
     const buttonStyle = {
@@ -72,82 +76,100 @@ function Features() {
     const [hoveredButtonProfile, setHoveredButtonProfile] = useState(false);
 
     return (
-        <Base>
+        <Base style={{ backgroundColor: 'transparent' }}>
             <div style={{
-                backgroundColor: '#f8f9fa',
-                minHeight: '100vh', 
+                backgroundImage: `url(${backgroundImg})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh',
                 display: 'flex',
-                alignItems: 'center', 
-                justifyContent: 'center', 
+                flexDirection: 'column',
+                color: '#fff',
             }}>
-                <Container style={containerStyle}>
-                    <header>
-                        <h1 style={headingStyle}>Welcome to our website! Thank you for registering. We hope you enjoy our services</h1>
-                        <h2 style={paragraphStyle}>Here are some of the services we offer:</h2>
-                    </header>
-                    <Row>
-                        <Col md="4">
-                            <Card
-                                style={hoveredCardCourse ? { ...cardStyle, ...cardHoverStyle } : cardStyle}
-                                onMouseEnter={() => setHoveredCardCourse(true)}
-                                onMouseLeave={() => setHoveredCardCourse(false)}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h3" style={cardTitleStyle}>Courses</CardTitle>
-                                    <CardText style={cardTextStyle}>Explore a variety of courses to enhance your skills.</CardText>
-                                    <Button
-                                        color="primary"
-                                        style={hoveredButtonCourse ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
-                                        onMouseEnter={() => setHoveredButtonCourse(true)}
-                                        onMouseLeave={() => setHoveredButtonCourse(false)}
-                                    >
-                                       <NavLink tag={ReactLink} to="/user/courses">Browse Courses</NavLink> 
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md="4">
-                            <Card
-                                style={hoveredCardFeedback ? { ...cardStyle, ...cardHoverStyle } : cardStyle}
-                                onMouseEnter={() => setHoveredCardFeedback(true)}
-                                onMouseLeave={() => setHoveredCardFeedback(false)}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h3" style={cardTitleStyle}>Feedback</CardTitle>
-                                    <CardText style={cardTextStyle}>We value your feedback to help us improve our services.</CardText>
-                                    <Button
-                                        color="secondary"
-                                        style={hoveredButtonFeedback ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
-                                        onMouseEnter={() => setHoveredButtonFeedback(true)}
-                                        onMouseLeave={() => setHoveredButtonFeedback(false)}
-                                    >
-                                       <NavLink tag={ReactLink} to="/user/feedback"> Provide Feedback</NavLink> 
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md="4">
-                            <Card
-                                style={hoveredCardProfile ? { ...cardStyle, ...cardHoverStyle } : cardStyle}
-                                onMouseEnter={() => setHoveredCardProfile(true)}
-                                onMouseLeave={() => setHoveredCardProfile(false)}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h3" style={cardTitleStyle}>Profile</CardTitle>
-                                    <CardText style={cardTextStyle}>View and update your profile information.</CardText>
-                                    <Button
-                                        color="secondary"
-                                        style={hoveredButtonProfile ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
-                                        onMouseEnter={() => setHoveredButtonProfile(true)}
-                                        onMouseLeave={() => setHoveredButtonProfile(false)}
-                                    >
-                                       <NavLink tag={ReactLink} to={`/user/dashboard/${object.user.data.id}`}>Your Profile</NavLink> 
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
+                <header style={{marginTop:'0.9%'}}>
+                    <center>
+                        <h1 style={headingStyle}>Welcome to our website! Thank you for registering.</h1>
+                        <h1 style={headingStyle}> We hope you enjoy our services</h1>
+                    </center>
+                </header>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '5%',
+                    backgroundColor: 'rgba(255, 255, 255, 0.0)',
+                    borderRadius: '8px',
+                }}>
+                    
+                    <Container style={containerStyle}>
+                        <header>
+                            <h2 style={paragraphStyle}>Here are some of the services we offer:</h2>
+                        </header>
+                        <Row>
+                            <Col md="4">
+                                <Card
+                                    style={hoveredCardProfile ? { ...cardStyle, ...cardHoverStyle } : cardStyle}
+                                    onMouseEnter={() => setHoveredCardProfile(true)}
+                                    onMouseLeave={() => setHoveredCardProfile(false)}
+                                   
+                                >
+                                    <CardBody>
+                                        <CardTitle tag="h3" style={cardTitleStyle}>Profile</CardTitle>
+                                        <CardText style={cardTextStyle}>View and update your profile information.</CardText>
+                                        <Button
+                                            color="secondary"
+                                            style={hoveredButtonProfile ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
+                                            onMouseEnter={() => setHoveredButtonProfile(true)}
+                                            onMouseLeave={() => setHoveredButtonProfile(false)}
+                                        >
+                                            <NavLink tag={ReactLink} to={`/user/dashboard/${object.user.data.id}`}>Your Profile</NavLink>
+                                        </Button>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <Col md="4">
+                                <Card
+                                    style={hoveredCardCourse ? { ...cardStyle, ...cardHoverStyle } : cardStyle}
+                                    onMouseEnter={() => setHoveredCardCourse(true)}
+                                    onMouseLeave={() => setHoveredCardCourse(false)}
+                                >
+                                    <CardBody>
+                                        <CardTitle tag="h3" style={cardTitleStyle}>Courses</CardTitle>
+                                        <CardText style={cardTextStyle}>Explore a variety of courses to enhance your skills.</CardText>
+                                        <Button
+                                            color="primary"
+                                            style={hoveredButtonCourse ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
+                                            onMouseEnter={() => setHoveredButtonCourse(true)}
+                                            onMouseLeave={() => setHoveredButtonCourse(false)}
+                                        >
+                                            <NavLink tag={ReactLink} to="/user/courses">Browse Courses</NavLink>
+                                        </Button>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <Col md="4">
+                                <Card
+                                    style={hoveredCardFeedback ? { ...cardStyle, ...cardHoverStyle } : cardStyle}
+                                    onMouseEnter={() => setHoveredCardFeedback(true)}
+                                    onMouseLeave={() => setHoveredCardFeedback(false)}
+                                >
+                                    <CardBody>
+                                        <CardTitle tag="h3" style={cardTitleStyle}>Feedback</CardTitle>
+                                        <CardText style={cardTextStyle}>We value your feedback to help us improve our services.</CardText>
+                                        <Button
+                                            color="secondary"
+                                            style={hoveredButtonFeedback ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
+                                            onMouseEnter={() => setHoveredButtonFeedback(true)}
+                                            onMouseLeave={() => setHoveredButtonFeedback(false)}
+                                        >
+                                            <NavLink tag={ReactLink} to="/user/feedback"> Provide Feedback</NavLink>
+                                        </Button>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </div>
         </Base>
     );
